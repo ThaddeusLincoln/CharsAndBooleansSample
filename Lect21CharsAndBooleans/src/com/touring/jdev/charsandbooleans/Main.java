49034a878f939bad77d04505d54d198207032031
ThaddeusLincolnStevens <beny1419@yahoo.es>
2016-08-02 23:53:33 +0200

    Simple use of unicode characters in a char

---
+++ Lect21CharsAndBooleans/src/com/touring/jdev/charsandbooleans/Main.java
@@ -0,0 +1,16 @@
+package com.touring.jdev.charsandbooleans;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// we can use a unicode character as literal in a char
+		char myChar = '\u00A9';
+		System.out.println("The char variable is containing : " + myChar);
+		
+		char registeredSymbol = '\u00AE';
+		System.out.println("The char variable is containing : " + registeredSymbol);
+
+	}
+
+}
